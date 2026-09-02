265914f9a47f51dbd93023bba57d54b797bf810e
nandrianina-git <rakotonirinafitahiana85@gmail.com>
2026-09-02 08:32:03 +0300

    correction gare
    correction gare

---
+++ mobile/.widget_preview/lib/src/dtd/dtd_connection_info.dart
@@ -0,0 +1,8 @@
+// ignore_for_file: implementation_imports
+
+const String kWidgetPreviewDtdUri = 'ws://127.0.0.1:62047/ykTBHgPHYz8=';
+const String kWidgetPreviewService =
+    'widget-preview-a08ddf4e-ad5a-4ed4-ae80-9ece8fe7ba19';
+const String kWidgetPreviewScaffoldStream =
+    'WidgetPreviewScaffold-a08ddf4e-ad5a-4ed4-ae80-9ece8fe7ba19';
+const String kProjectRootPath = r'D:\Bossy\stage\cooperative\mobile';
